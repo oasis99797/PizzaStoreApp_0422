20a81304e87a856f0b38472b34dff3117f37d937
oasis99797 <oasis99797@gmail.com>
2020-04-23 20:09:52 +0900

    권한 허용시 실제 전화 연결

--- app/src/main/java/com/bklee/pizzastoreapp_0422/StoreDetailActivity.kt
+++ app/src/main/java/com/bklee/pizzastoreapp_0422/StoreDetailActivity.kt
@@ -1,8 +1,16 @@
 package com.bklee.pizzastoreapp_0422
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bklee.pizzastoreapp_0422.datas.Store
+import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import kotlinx.android.synthetic.main.activity_store_detail.*
 
 class StoreDetailActivity : BaseActivity() {
 
@@ -16,6 +24,35 @@ class StoreDetailActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+
+        callToStoreBtn.setOnClickListener {
+            //            it 변수 활용 예시
+//            it.visibility = View.GONE
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+                    val storeCallUri = Uri.parse("tel:${storeData.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, storeCallUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "통화 권한을 허가해줘야 사용가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setDeniedMessage("통화 권한 설정을 해야 사용 가능합니다. [설정] 에서 권한을 넣어주세요.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+
+        }
 
     }
 
